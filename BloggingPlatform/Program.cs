c7785126c47c28a3bc71b8c9e62a61e3a5af2c66
kishore <imkishor16@gmail.com.com>
2025-07-15 07:12:59 +0530

    fixed erros

--- BloggingPlatform/Program.cs
+++ BloggingPlatform/Program.cs
@@ -19,6 +19,8 @@ using System.Text;
 using BloggingPlatform.Filters;
 using BloggingPlatform.Middleware;
 using System.Threading.RateLimiting;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 var builder = WebApplication.CreateBuilder(args);
 
--- BloggingPlatform/obj/Debug/net9.0/BloggingPlatform.AssemblyInfo.cs
+++ BloggingPlatform/obj/Debug/net9.0/BloggingPlatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloggingPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f8622f17af56ef5781aae354a845105b597582c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+170bcddea655d7b2137d4246602807ac154102d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloggingPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloggingPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
